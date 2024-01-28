f1f122b06016e3fc753a0b61a77a71c9201c521a
unknown <carltan2700@gmail.com>
2024-01-28 20:04:24 +0800

    just finished adding css and html

--- index.php
+++ index.php
@@ -4,23 +4,52 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Bug Report</title>
+        <style>
+            form{
+                outline: 1px solid red;
+                width: 275px;
+                height: 420px;
+                padding: 2% 3%;
+                margin: 4% auto;
+                border-radius: 4%;
+            }
+            h1{
+                text-align: center;
+                margin-top: 0;
+            }
+            label{
+                display: block;
+                margin: 2% 0;
+            }
+            input{
+                margin-left: 6%;
+            }
+            #first_name{
+                margin-left: 6.7%;
+            }
+            #last_name{
+                margin-left: 7.4%;
+            }
+            #email{
+                margin-left: 19%;
+            }
+            #issue_title{
+                margin-left: 8.8%;
+            }
+
+        </style>
     </head>
     <body>
-        <form action="process.php">
+        <form action="process.php" method='post'>
             <h1>Bug Report</h1>
-            <label for="date">Date Today:</label>
-            <input type="datetime" name='date' id='date'>
-            <label for="first">First Name:</label>
-            <input type="input" name='first_name' id='first'>
-            <label for="last">Last Name:</label>
-            <input type="input" name='last_name' id='last'>
-            <label for="email">Email:</label>
-            <input type="input" name='email' id='email'>
-            <label for="issue_title">Issue Title:</label>
-            <input type="input" name='issue_title' id='issue_title'>
-            <label for='details'>Issue Details:</label>
-            <textarea name="details" id="" cols="30" rows="10"></textarea>
+            <label>Date Today:<input type="date" name='date'></label>    
+            <label>First Name:<input type="input" name='first_name' id='first_name'></label>            
+            <label>Last Name:<input type="input" name='last_name' id='last_name'></label>          
+            <label>Email:<input type="input" name='email' id='email'></label>            
+            <label>Issue Title:<input type="input" name='issue_title' id='issue_title'></label>           
+            <label>Issue Details:</label>
+            <textarea name="details" id="" cols="34" rows="10"></textarea>
         </form>
     </body>
 </html>
